c30a3b4f60263ab873c5b09dbd01e19e6a608d6e
Jaiden McNamara <jaidenm78@icloud.com>
2026-06-30 13:01:05 -0400

    Add asset unload support

--- src/assetCache.ts
+++ src/assetCache.ts
@@ -1,27 +1,27 @@
 export class AssetCache {
-
   loadedAssets: Set<string>;
 
   constructor() {
-
     this.loadedAssets = new Set();
-
   }
 
   loadAsset(file: string): void {
-
     if (this.loadedAssets.has(file)) {
-
       console.log(`Asset already loaded: ${file}`);
-
       return;
-
     }
 
     console.log(`Loading asset: ${file}`);
-
     this.loadedAssets.add(file);
-
   }
 
+  unloadAsset(file: string): void {
+    if (!this.loadedAssets.has(file)) {
+      console.log(`Asset not loaded: ${file}`);
+      return;
+    }
+
+    console.log(`Unloading asset: ${file}`);
+    this.loadedAssets.delete(file);
+  }
 }
